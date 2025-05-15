7fe969f1776d858412054044012f48ddd0aec366
@devnbugs <devnbugs@gmail.com>
2025-05-15 17:19:06 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:wordpress_app/config/language_config.dart';
 import 'package:wordpress_app/services/app_service.dart';
 import 'app.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await MobileAds.instance.initialize(); // <-- Initialize AdMob
+
   await AppService.appStartupFunctions();
+
   runApp(EasyLocalization(
     supportedLocales: LanguageConfig.supportedLocales,
     path: 'assets/translations',
