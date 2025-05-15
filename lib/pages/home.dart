f71a99b9d026e3e3694c66e2408ee100749e6a17
@devnbugs <devnbugs@gmail.com>
2025-05-15 17:32:03 +0100

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,27 +3,29 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:provider/provider.dart';
+
 import 'package:wordpress_app/blocs/ads_bloc.dart';
 import 'package:wordpress_app/blocs/category_bloc.dart';
 import 'package:wordpress_app/blocs/config_bloc.dart';
 import 'package:wordpress_app/blocs/settings_bloc.dart';
 import 'package:wordpress_app/blocs/user_bloc.dart';
+import 'package:wordpress_app/blocs/featured_bloc.dart';
+import 'package:wordpress_app/blocs/latest_articles_bloc.dart';
+import 'package:wordpress_app/blocs/notification_bloc.dart';
+import 'package:wordpress_app/blocs/popular_articles_bloc.dart';
+
 import 'package:wordpress_app/config/ad_config.dart';
 import 'package:wordpress_app/services/notification_service.dart';
+import 'package:wordpress_app/services/app_links_service.dart';
+import 'package:wordpress_app/services/ad_manager.dart';
+
 import 'package:wordpress_app/tabs/home_tab.dart';
 import 'package:wordpress_app/tabs/home_tab_without_tabs.dart';
 import 'package:wordpress_app/tabs/profile_tab.dart';
 import 'package:wordpress_app/tabs/search_tab.dart';
 import 'package:wordpress_app/tabs/video_tab.dart';
-import '../blocs/featured_bloc.dart';
-import '../blocs/latest_articles_bloc.dart';
-import '../blocs/notification_bloc.dart';
-import '../blocs/popular_articles_bloc.dart';
-import '../services/app_links_service.dart';
-import '../tabs/bookmark_tab.dart';
-import 'package:provider/provider.dart';
-import 'package:wordpress_app/services/ad_manager.dart';
-
+import 'package:wordpress_app/tabs/bookmark_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -34,11 +36,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
-  PageController? _pageController;
+  late PageController _pageController;
+  int _tapCount = 0;
 
-  final List<IconData> _iconList = [Feather.home, Feather.search, Feather.heart, Feather.user];
+  final List<IconData> _iconList = [
+    Feather.home,
+    Feather.search,
+    Feather.heart,
+    Feather.user,
+  ];
 
-  final List<IconData> _iconListWithVideoTab = [Feather.home, Feather.youtube, Feather.search, Feather.heart, Feather.user];
+  final List<IconData> _iconListWithVideoTab = [
+    Feather.home,
+    Feather.youtube,
+    Feather.search,
+    Feather.heart,
+    Feather.user,
+  ];
 
   final List<Widget> _tabs = [
     const SearchTab(),
@@ -46,7 +60,26 @@ class _HomePageState extends State<HomePage> {
     const SettingPage(),
   ];
 
-  final List<Widget> _tabsWithVideo = [const VideoTab(), const SearchTab(), const BookmarkTab(), const SettingPage()];
+  final List<Widget> _tabsWithVideo = [
+    const VideoTab(),
+    const SearchTab(),
+    const BookmarkTab(),
+    const SettingPage(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    _initData();
+    _fetchPostsData();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   void _initData() async {
     final cb = context.read<CategoryBloc>();
@@ -57,6 +90,7 @@ class _HomePageState extends State<HomePage> {
     final configs = context.read<ConfigBloc>().configs!;
 
     NotificationService().initOneSignal(context);
+
     Future.microtask(() {
       nb.checkPermission();
       cb.fetchData(configs.blockedCategories);
@@ -68,8 +102,9 @@ class _HomePageState extends State<HomePage> {
     if (!ub.guestUser) {
       ub.getUserData();
     }
+
     if (configs.admobEnabled) {
-      await AdConfig.initAdmob().then((value) {
+      await AdConfig.initAdmob().then((_) {
         if (configs.interstitialAdsEnabled) {
           ab.initiateAds();
         }
@@ -77,7 +112,7 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  _fetchPostsData() async {
+  void _fetchPostsData() {
     Future.microtask(() {
       if (!mounted) return;
       final configs = context.read<ConfigBloc>().configs!;
@@ -91,102 +126,92 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController();
-    _initData();
-    _fetchPostsData();
-  }
-
-  @override
-  void dispose() {
-    _pageController!.dispose();
-    super.dispose();
-  }
-
-  void onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    _pageController!.animateToPage(index, duration: const Duration(milliseconds: 200), curve: Curves.easeIn);
-  }
-
-  Future _onBackPressed() async {
-    if (_selectedIndex != 0) {
-      setState(() => _selectedIndex = 0);
-      _pageController!.animateToPage(0, duration: const Duration(milliseconds: 200), curve: Curves.easeIn);
-    } else {
-      await SystemChannels.platform.invokeMethod<void>('SystemNavigator.pop', true);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final cb = context.read<ConfigBloc>();
-    return PopScope(
-      onPopInvokedWithResult: (didPop, x) {
-        if (didPop) {
-          return;
-        }
-        _onBackPressed();
-      },
-      child: Scaffold(
-        bottomNavigationBar: _bottonNavigationBar(context, cb),
-        body: PageView(
-          physics: const NeverScrollableScrollPhysics(),
-          allowImplicitScrolling: false,
-          controller: _pageController,
-          children: <Widget>[
-            cb.configs!.homeCategories.isEmpty
-                ? HomeTabWithoutTabs(
-                    configs: cb.configs!,
-                  )
-                : HomeTab(
-                    configs: cb.configs!,
-                    homeCategories: cb.homeCategories,
-                    categoryTabs: _categoryTabs(cb),
-                  ),
-            ..._childrens(cb)
-          ],
-        ),
-      ),
+  void _onItemTapped(int index) {
+    setState(() => _selectedIndex = index);
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeIn,
     );
   }
 
-  List<Widget> _childrens(ConfigBloc cb) {
-    if (cb.configs!.videoTabEnabled) {
-      return _tabsWithVideo;
+  Future<bool> _onBackPressed() async {
+    if (_selectedIndex != 0) {
+      setState(() => _selectedIndex = 0);
+      _pageController.animateToPage(
+        0,
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.easeIn,
+      );
+      return false;
     } else {
-      return _tabs;
+      await SystemChannels.platform.invokeMethod<void>('SystemNavigator.pop');
+      return true;
     }
   }
 
-  List<Tab> _categoryTabs(ConfigBloc cb) {
-    return cb.homeCategories.map((e) => Tab(text: e.name)).toList()..insert(0, Tab(text: 'explore'.tr()));
+  void _onUserTap() {
+    _tapCount++;
+    if (_tapCount >= 10) {
+      _tapCount = 0;
+      AdManager.showRewardedAd(() {
+        debugPrint("User watched the ad and earned a reward.");
+      });
+    }
   }
 
-  AnimatedBottomNavigationBar _bottonNavigationBar(BuildContext context, ConfigBloc cb) {
+  List<Widget> _childrens(ConfigBloc cb) {
+    return cb.configs!.videoTabEnabled ? _tabsWithVideo : _tabs;
+  }
+
+  List<Tab> _categoryTabs(ConfigBloc cb) {
+    return [
+      Tab(text: 'explore'.tr()),
+      ...cb.homeCategories.map((e) => Tab(text: e.name)),
+    ];
+  }
+
+  AnimatedBottomNavigationBar _bottomNavigationBar(
+      BuildContext context, ConfigBloc cb) {
     return AnimatedBottomNavigationBar(
       icons: cb.configs!.videoTabEnabled ? _iconListWithVideoTab : _iconList,
       gapLocation: GapLocation.none,
       activeIndex: _selectedIndex,
       iconSize: 22,
-      backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
-      activeColor: Theme.of(context).bottomNavigationBarTheme.selectedItemColor,
-      inactiveColor: Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
+      backgroundColor:
+          Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+      activeColor:
+          Theme.of(context).bottomNavigationBarTheme.selectedItemColor,
+      inactiveColor:
+          Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
       splashColor: Theme.of(context).primaryColor,
-      onTap: (index) => onItemTapped(index),
+      onTap: _onItemTapped,
     );
   }
-  int _tapCount = 0;
 
-void _onUserTap() {
-  _tapCount++;
-  if (_tapCount >= 10) {
-    _tapCount = 0;
-    AdManager.showRewardedAd(() {
-      print("User watched the ad and earned a reward.");
-    });
+  @override
+  Widget build(BuildContext context) {
+    final cb = context.watch<ConfigBloc>();
+
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: Scaffold(
+        bottomNavigationBar: _bottomNavigationBar(context, cb),
+        body: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: _pageController,
+          children: [
+            cb.configs!.homeCategories.isEmpty
+                ? HomeTabWithoutTabs(configs: cb.configs!)
+                : HomeTab(
+                    configs: cb.configs!,
+                    homeCategories: cb.homeCategories,
+                    categoryTabs: _categoryTabs(cb),
+                  ),
+            ..._childrens(cb),
+          ],
+        ),
+      ),
+    );
   }
 }
